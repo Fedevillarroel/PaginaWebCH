756831b177bda8942a84c38ca3dec672f7451cf1
Fedevillarroel <fedeavillarroel@gmail.com>
2026-06-09 13:26:32 -0300

    fix: ajuste formato url whatsapp

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -3,10 +3,10 @@ import './WhatsAppButton.css';
 
 const WhatsAppButton = () => {
   return (
-    <a 
-      href="https://wa.me/5493435348834" 
-      target="_blank" 
-      rel="noopener noreferrer" 
+    <a
+      href="https://wa.me/+5493435348834"
+      target="_blank"
+      rel="noopener noreferrer"
       className="whatsapp-float"
       aria-label="Chat on WhatsApp"
     >
